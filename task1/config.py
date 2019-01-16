a29a73537147106fc7acd42befa8c2b783c8517e
Axtux <axtux@hotmail.com>
2019-01-16 03:55:53 +0100

    adjust params

--- task1/config.py
+++ task1/config.py
@@ -4,6 +4,12 @@ PWD = 's0oObGX2oIZeGZ8'
 HOST = '192.168.0.174'
 DB = 'employees'
 
-N_QUERIES = 420
-THRESHOLD = 20
-LAMBDAS = [0.01, 0.1, 0.15, 0.2, 0.3, 0.5, 1]
+#"""
+N_QUERIES = 450
+THRESHOLD = 50
+LAMBDAS = [0.03, 0.1, 0.15, 0.2, 0.3, 0.5, 1]
+"""
+N_QUERIES = 10
+THRESHOLD = 2
+LAMBDAS = [0.1, 0.15]
+#"""
