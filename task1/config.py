fde0b96fe03b96f1b9bad10182e9e8f4e8e6db1b
Axtux <axtux@hotmail.com>
2019-01-16 04:42:26 +0100

    faster

--- task1/config.py
+++ task1/config.py
@@ -5,11 +5,11 @@ HOST = '192.168.0.174'
 DB = 'employees'
 
 #"""
-N_QUERIES = 450
+N_QUERIES = 150
 THRESHOLD = 50
 LAMBDAS = [0.03, 0.1, 0.15, 0.2, 0.3, 0.5, 1]
 """
-N_QUERIES = 10
+N_QUERIES = 5
 THRESHOLD = 2
-LAMBDAS = [0.1, 0.15]
+LAMBDAS = [0.1, 0.15, 0.2]
 #"""
