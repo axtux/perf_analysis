a25ea5802330052ed42086c6bbf7a6f7d8eb7423
Axtux <axtux@hotmail.com>
2019-01-16 03:55:33 +0100

    errors and LaTeX tab

--- task1/experiment.py
+++ task1/experiment.py
@@ -11,20 +11,39 @@ def experiment():
 	log('task 1')
 	colors = ['tomato', 'darkslateblue', 'darkturquoise', 'darkgreen']
 
+	a_qps = {}
+	a_avg = {}
+	a_errors = {}
+	# create one tab per query
+	for q in query.QUERIES:
+		a_qps[q] = []
+		a_avg[q] = []
+		a_errors[q] = []
+
 	plt.figure()
 	for l in config.LAMBDAS:
 		print('Currently proceding to lambda: '+str(l))
 		wait_times = np.random.exponential(scale=l,size=config.N_QUERIES)
-		for q, c in zip(query.QUERIES, colors):
-			label = '\tQuery type '+str(q)
-			print(label+': ', end='', flush=True)
+		for q in query.QUERIES:
+			print('\tQuery type '+str(q)+': ', end='', flush=True)
 			# making queries
-			(queries_per_second, res_time) = query.threaded_queries(q, wait_times, config.THRESHOLD)
-			print('{:.3f}q/s, avg {:.3f}s'.format(queries_per_second, res_time))
+			(qps, min, avg, max) = query.threaded_queries(q, wait_times, config.THRESHOLD)
+			print('q/s, min, avg, max = {:.3f}, {:.3f}, {:.3f}, {:.3f}'.format(qps, min, avg, max))
 			# TODO add error bars https://matplotlib.org/api/_as_gen/matplotlib.pyplot.errorbar.html
-			plt.plot(queries_per_second, res_time, c=c, marker='.', label=label)
+			a_qps[q].append(qps)
+			a_avg[q].append(avg)
+			a_errors[q].append((avg-min, max-avg))
 			# let server catch up
-			time.sleep(5)
+			time.sleep(2)
+
+	for q, c in zip(query.QUERIES, colors):
+		plt.errorbar(a_qps[q], a_avg[q], c=c, marker='.', label='Query type '+str(q), yerr=a_errors[q])
+	print('\nLaTeX tab:')
+	for l, i in zip(config.LAMBDAS, range(len(config.LAMBDAS))):
+		print('\t'+str(l)+' & '+str('{:.3f}'.format(a_qps[1][i])), end='')
+		for q in query.QUERIES:
+			print(' & '+str('{:.3f}'.format(a_avg[q][i])), end='')
+		print('\\\\')
 
 	plt.ylabel('Average response time')
 	plt.xlabel('Queries per second')
